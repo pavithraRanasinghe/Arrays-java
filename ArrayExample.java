4e4cf0ed905122d00cdb4d2507a975cb6511c90b
PavithraRanasinghe <paviranasinghe20@gamil.com>
2019-03-09 08:18:26 +0530

    final initialized

--- ArrayExample.java
+++ ArrayExample.java
@@ -2,11 +2,15 @@ class ArrayExample{
 	public static void main(String args[]){
 		String[] names = {"Pavithra","Anuradha","Sameera","Nirupama"};
 		String[] cars = {"BMW","VOLVO","LAMBO"};
-		printFirstName(names);
+		printFirstElement(cars);
+		printSecondElement(names);
 		
 	}
-	private static void printFirstName(String names[]){
-		System.out.println("First name is : "+names[0]);
+	private static void printFirstElement(String []x){
+		System.out.println("First element is : "+x[0]);
 	}
-	
+	public static void printSecondElement(String []x){
+		System.out.println("Second element is : "+x[1]);
+	}
+		
 }
