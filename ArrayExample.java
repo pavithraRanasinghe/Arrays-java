eb0f4c170718cab0920dfa23b197d1f88097ee06
PavithraRanasinghe <paviranasinghe20@gamil.com>
2019-03-09 08:03:12 +0530

    Second array initialized

--- ArrayExample.java
+++ ArrayExample.java
@@ -1,5 +1,12 @@
 class ArrayExample{
 	public static void main(String args[]){
+		String[] names = {"Pavithra","Anuradha","Sameera","Nirupama"};
+		String[] cars = {"BMW","VOLVO","LAMBO"};
+		printFirstName(names);
 		
 	}
+	private static void printFirstName(String names[]){
+		System.out.println("First name is : "+names[0]);
+	}
+	
 }
